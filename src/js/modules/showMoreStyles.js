dff49dd25d356eafd7bd23b2660978d01aa0b06e
Alex <alexey.officetime@gmail.com>
2022-08-16 17:46:05 +0300

    END

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,47 @@
+import {getResourse} from "../services/request";
+
+const showMoreStyles = (trigger, wrapper) =>{
+    const btn = document.querySelector(trigger);
+    
+/*     cards.forEach(card =>{
+        card.classList.add('animated', 'fadeInUp');
+
+    });
+    btn.addEventListener('click', () =>{
+        cards.forEach(card =>{
+            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs' );
+            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+        });
+        btn.style.display = 'none';
+        btn.remove();
+    }); */
+
+    btn.addEventListener('click', function() {
+        getResourse('http://localhost:3000/styles')
+            .then(res => createCards(res))
+            .catch(error => console.log(error));
+
+        this.remove();
+    });
+
+    function createCards(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1', 'animated', 'fadeInUp');
+
+            card.innerHTML = `
+                <div class='styles-block'>
+                    <img src=${item.src} alt>
+                    <h4>${item.title}</h4>
+                    <a href=${item.link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+
+        });
+    }
+};
+export default showMoreStyles;
+
